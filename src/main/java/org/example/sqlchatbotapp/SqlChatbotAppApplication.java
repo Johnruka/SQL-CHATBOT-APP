dc94fb4caf7fe68a6af3c12b9a0f0cdbc6fb9e69
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2025-01-07 19:01:36 +0100

    Initial commit

---
+++ src/main/java/org/example/sqlchatbotapp/SqlChatbotAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.sqlchatbotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlChatbotAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlChatbotAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/sqlchatbotapp/SqlChatbotAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sqlchatbotapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlChatbotAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
